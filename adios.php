4889579c6195da5f4f3106c32aab61952c04f70a
Josemi84 <josemicasadovalle@hotmail.com>
2021-10-28 06:48:12 +0200

    Subiendo archivo caso 3

---
+++ adios.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Caso 2 Unidad 2</title>
+    </head>
+    
+    <body>
+        <?php echo 'Hasta luego desarrollador!!!'; ?>
+    </body>
+</html>
